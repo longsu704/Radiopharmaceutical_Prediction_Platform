59011edef18c96a8a679d1c8985ad0f0dd9e9985
longsu704 <1561371742@qq.com>
2026-04-08 10:08:14 +0800

    Update EFM.py

--- EFM.py
+++ EFM.py
@@ -16,7 +16,7 @@ except ImportError:
 warnings.filterwarnings('ignore')
 
 # 加载数据
-train_file_path = r'D:\wordjihe\Biodistribution_data_radiopharmaceuticals.xlsx'  # 请修改为你的路径
+train_file_path = 'Biodistribution_data_radiopharmaceuticals.xlsx' 
 df_train = pd.read_excel(train_file_path, na_values=['NA', 'na', 'N/A', 'n/a', ''])
 
 X_train_origin = df_train.iloc[:, 0:13].copy()
